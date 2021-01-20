c53aa946f949022d17a89af2d2629c98415272ec
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-20 19:06:38 +0600

    add text stylization

---
+++ scripts.js
@@ -0,0 +1,43 @@
+
+let result = 0;
+
+//question
+let answer1 = prompt(
+    "what is  the  offical name of Java scripts"
+);
+let answer2 = prompt(
+    "what html stands far? "
+)
+let answer3 = prompt(
+   "Which html tag is used for paragraphs?" 
+)
+if(answer3 == "Hav many lakes are in Kg"){
+    result = result + 5;
+
+
+   
+    console.log(" Your've earned " + result + " point ");
+}
+if(answer2 == "css"){
+    result = result + 5;
+
+
+   
+    console.log(" Your've earned " + result + " point ");
+}
+if (answer1 == "ECMAScript"){
+    result = result + 5;
+
+
+    //show  results
+    console.log(" Your've earned " + result + " point ");
+}
+let answer5 = confirm("Is Kyrgystan a presidetial republic?");
+if(answer5){
+    result = result + 5;
+
+
+    //show  results
+    console.log(" Your've earned " + result + " point ");
+}
+let answer4 = confirm("")
