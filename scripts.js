6327e9742a8d82a81b14004a53cf5d97eb31ef25
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-21 13:54:25 +0600

    add answer7

--- scripts.js
+++ scripts.js
@@ -1,40 +1,4 @@
-// let result0 = 0;
-// let answer4 = confirm("Омоним  айтылышы бирдей , жазылышы бирдей , маниси арбашка?");
-// if(answer4){
-    
 
-//     console.log(" Your've earned " + result1 + " point  " );
-
-// }
-
-
-// let answer3 = confirm("Антоним  айтылышы бирмес , жазылышы бирмес , маниси окшош? ");
-// if(answer3){
-//     result2 = result + 10;
-
-//     console.log(" Your've earned " + result2 + " point  " );
-// }
-
-// let answer2 = confirm("Табиятынтынан карама-каршы создор сенонимдер?");
-// if(answer2){
-//     result3 = result + 10;
-
-//     console.log(" Your've earned " + result3 + " point  " );
-// }
-
-// let answer5 = confirm(" Токтогул.Сатылганов атындагы мектеп 1991-жылы негизделген?");
-// if(answer5){
-//     result5 = result + 10;
-
-//     console.log(" Your've earned " + result5 + " point  " );
-// }
-
-// let answer6 = confirm(" Ата-Мекендик согуш качан болгон 1992 ?");
-// if(answer6){
-//     result6 = result + 10;
-
-//     console.log(" Your've earned " + result6 + " point  " );
-// }
 let result = 0;
 let answer4 = confirm("Омоним  айтылышы бирдей , жазылышы бирдей , маниси арбашка?");
 if(answer4){
@@ -63,6 +27,13 @@ if(answer5){
 let answer6 = confirm(" Ата-Мекендик согуш качан болгон 1992 ?");
 if(answer6){
     result6 = result + 10;}
+
+
+    let answer7 = confirm("Ысык-кол кыргызстандабы?");
+    if(answer7){
+        result7 = result + 10;}
+    
+   
 
 
 
