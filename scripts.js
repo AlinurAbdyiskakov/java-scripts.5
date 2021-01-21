fac6fade56d294094e56200d49052fca997ccfa6
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-21 14:04:18 +0600

    add new consol log

--- scripts.js
+++ scripts.js
@@ -1,44 +1,45 @@
 
 let result = 0;
 let answer4 = confirm("Омоним  айтылышы бирдей , жазылышы бирдей , маниси арбашка?");
-if(answer4){
-    result1 = result + 10;
-    console.log(" Your've earned " + result1 + " point  " );
+if (answer4) {
+    result = result + 10;
+
 }
 let answer3 = confirm("Антоним  айтылышы бирмес , жазылышы бирмес , маниси окшош? ");
-if(answer3){
-    result2 = result + 10;
-    console.log(" Your've earned " + result2 + " point  " );
+if (answer3) {
+    result = result + 30;
+
 }
 let answer2 = confirm("Табиятынтынан карама-каршы создор сенонимдер?");
-if(answer2){
-    result3 = result + 10;
-    console.log(" Your've earned " + result3 + " point  " );
+if (answer2) {
+    result = result + 15;
+
 }
 
 let answer5 = confirm(" Токтагул Сатылганов атындагы мектеп 1991-жылы негизделген?");
 
-if(answer5){
-    result5 = result + 10;
+if (answer5) {
+    result = result + 20;
 
-    console.log(" Your've earned " + result5 + " point  " );
+
 }
 
 let answer6 = confirm(" Ата-Мекендик согуш качан болгон 1992 ?");
-if(answer6){
-    result6 = result + 10;}
+if (answer6) {
+    result = result + 10;
+}
 
 
-    let answer7 = confirm("Ысык-кол кыргызстандабы?");
-    if(answer7){
-        result7 = result + 10;
-    console.log(" Your ve earned " + result5 + " point");}
-    
-    let answer8 = confirm("Каракол Ысык-Колдун борбору ?");
-    if(answer8){
-        result8 = result + 10;
-    console.log(" Your ve earned " + result8 + " point");}
-   
+let answer7 = confirm("Ысык-кол кыргызстандабы?");
+if (answer7) {
+    result = result + 5;
+}
+
+let answer8 = confirm("Каракол Ысык-Колдун борбору ?");
+if (answer8) {
+    result = result + 10;
+}
+console.log(" Your ve earned " + result + " point")
 
 
 
